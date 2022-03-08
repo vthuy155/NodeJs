99e031bc6ea557fb95e5044de9d228a31afc06f8
Huy <huyvtph13755@fpt.edu.vn>
2022-03-08 18:27:45 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// B1: include thư viện http 
+const http = require('http');
+// B2: khởi tạo server
+const server = http.createServer((req, res) => {
+    // console.log(req.url);
+    // console.log("Hello");
+    if(req.url === '/'){
+        // console.log("HomePage");
+        res.setHeader('Content-Type', 'text/html');
+        res.write("<h1>Home Page</h1>")
+        res.end;
+    }
+    if(req.url === '/products'){
+        // console.log("Product Page");
+        res.setHeader('Content-Type', 'text/html');
+        res.write("<h1>Product Page</>");
+        res.end;
+    }
+    if(req.url === '/news'){
+        // console.log("Product Page");
+        res.setHeader('Content-Type', 'text/html');
+        res.write("<h1>News Page</>");
+        res.end;
+    }
+});
+// B3: Lắng nghe cổng thực thi
+const port = 3001;
+server.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+});
